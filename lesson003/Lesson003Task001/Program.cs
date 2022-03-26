b79845f7c2f2abf69625e289d383452a322f9fa4
Viktorm <vm@viktorm.ru>
2022-03-26 12:28:15 +0300

    Add task 001 for lesson 003

---
+++ lesson003/Lesson003Task001/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Lesson003Task001
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            BankAccount account1 = new BankAccount(100);
+            BankAccount account2 = new BankAccount();
+
+            account2.TransferMoney(account1, 50);
+
+            Console.WriteLine($"acc1: {account1.Balance}");
+            Console.WriteLine($"acc2: {account2.Balance}");
+        }
+    }
+
+    public class BankAccount
+    {
+        public int Balance { get; private set; }
+
+        public BankAccount()
+        {
+            Balance = 0;
+        }
+
+        public BankAccount(int balance)
+        {
+            Balance = balance;
+        }
+
+        public void TransferMoney(BankAccount accountFrom, int sum)
+        {
+            accountFrom.Balance -= sum;
+            Balance += sum;
+        }
+    }
+}
