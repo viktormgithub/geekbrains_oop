b87666f0fb8e9099e8ba82b4416a66b4a9c85b52
Viktorm <vm@viktorm.ru>
2022-03-26 14:59:50 +0300

    Add task 002 for lesson 003

---
+++ lesson003/Lesson003Task002/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lesson003Task002
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(ReverseString("microsoft"));
+        }
+
+        private static string ReverseString(string input)
+        {
+            char[] arrChars = input.ToCharArray();
+
+            // Можно использовать Array.Reverse(arr)
+            // Для учебных целей написал for
+            char[] arrCharReverse = new char[arrChars.Length];
+            for (int i = arrChars.Length - 1, c = 0; i >= 0; i--, c++)
+            {
+                arrCharReverse[c] = arrChars[i];
+            }
+
+            return new string(arrCharReverse);
+        }
+    }
+}
